3176786e837e9e9ef669a03c4109a342d2664b67
blakereeves19 <blake.reeves19@gmail.com>
2018-06-23 15:40:14 -0500

    Added variables

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+var firstName = 'Blake';
+let lastName = 'Reeves';
+
+firstName = "Will";
+lastName = "Johnston";
+
+const fullName = 'Blake Reeves';
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
